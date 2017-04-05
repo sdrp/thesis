e4aeff0fb2af35324a8e358023b7ff082b306a4b
Sudheesha Perera <sudheeshaperera@gmail.com>
2017-04-05 13:09:09 -0400

    ran sanity check experiment

--- vis_tractus_results.r
+++ vis_tractus_results.r
@@ -1,6 +1,6 @@
 # Visualize Tractus Results
 library(ggplot2)
-TractusResults <- read.table(file = 'output_file.tsv', sep = '\t', header = TRUE)
+TractusResults <- read.table(file = 'output_file_experiment.tsv', sep = '\t', header = TRUE)
 TractusResults$Expression.Level <- as.factor(TractusResults$Expression.Level)
 TractusResults$Predicted.Expression.Level <- as.factor(TractusResults$Predicted.Expression.Level)
 p <- ggplot(TractusResults, aes(x=Expression.Level, y=Expression.Value, color=Predicted.Expression.Level)) + 
