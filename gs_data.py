1b31ae40936af0dfc3099fb9a84a2bf0214dd876
Sudheesha Perera <sudheeshaperera@gmail.com>
2017-04-06 00:42:03 -0400

    tract specific hap simulation

--- gs_data.py
+++ gs_data.py
@@ -17,6 +17,7 @@
 
 import sys
 import random 
+import numpy
 
 # Parse command line arguments
 try:
@@ -36,20 +37,72 @@ f = open(output_file_path, 'w')
 ### Define Expression Functions ###
 # Which function is called is based on command line argument
 def random_fn(input_hap, noise):
-	return random.random()
+	# Generate expression randomly between 0 and 10
+	return random.random() * 10
+
+def gaussian_fn(input_hap, noise):
+	# Generates values from a Gausian distribution 
+	# with parameters mu and sigma
+	mu = 5 # mean
+	sigma = 1 # standard deviation
+	return max(0, numpy.random.normal(mu, sigma))
 
 
 # Choose the specified expression function
+tract_dependent = False
 if exp_fn_name == 'rand':
 	exp_fn = random_fn
+elif exp_fn_name == 'sanity_check':
+	exp_fn = sanity_check_fn
+elif exp_fn_name == 'tract_dependent':
+	tract_dependent = True
+elif exp_fn_name == 'snp_dependent':
+	exp_fn = snp_fn
+elif exp_fn_name == 'gaussian':
+	exp_fn = gaussian_fn
 else:
 	exp_fn = random_fn
 
-# Write to the output file
-for i in range(num_haps):
-	hap_str = ''.join(str(x)[0] for x in [round(random.random()) for k in range(hap_len)])
-	exp_val = str(exp_fn(hap_str, noise))
-	f.write(hap_str + '\t' + exp_val + '\n')
+
+# Generate random haps
+haps = [[round(random.random()) for k in range(hap_len)] for x in range(num_haps)]
+exp_vals = [0 for x in range(num_haps)]
+
+
+if tract_dependent:
+	# Generate the haps to have tract dependent expression
+	# Define a number of classes we want to emulate
+	K = 5
+	# Determine a tract-associated expression boost
+	tract_val = 10
+	# Next determine a tract length
+	tract_len = int((hap_len * 0.67) / K) # arbitrary proportion
+	# Get tract start points
+	tract_starts = [k*(hap_len/K) for k in range(K)]
+	# Get generate the tract signatures (ie. the tracts themselves)
+	tracts = [[round(random.random()) for i in range(tract_len)] for k in range(K)]
+	# Now inject the tracts into the haplotypes
+	for i in range(num_haps):
+		# Pick a random expression level for this hap 
+		exp_level = random.randint(0, K-1)
+		# Place modify the hap to have the tract
+		hap = haps[i]
+		hap[tract_starts[exp_level]:tract_starts[exp_level]+tract_len] = tracts[exp_level]
+		# Generate an expression value
+		exp_vals[i] = (random.random()*10) + (exp_level*tract_val)
+	# Write the tracts and expression to file
+	for i in range(num_haps):
+		hap = haps[i]
+		hap_str = ''.join(str(x)[0] for x in hap)
+		f.write(hap_str + '\t' + str(exp_vals[i]) + '\n')
+
+else:
+	# Write to the output file
+	for i in range(num_haps):
+		hap = haps[i]
+		exp_val = exp_fn(hap, noise)
+		hap_str = ''.join(str(x)[0] for x in hap)
+		f.write(hap_str + '\t' + str(exp_val) + '\n')
 
 # Close the output file
 f.close()
--- vis_tractus_results.r
+++ vis_tractus_results.r
@@ -1,10 +1,18 @@
 # Visualize Tractus Results
 library(ggplot2)
-TractusResults <- read.table(file = 'output_file_experiment.tsv', sep = '\t', header = TRUE)
+TractusResults <- read.table(file = 'tract_dep_results.tsv', sep = '\t', header = TRUE)
 TractusResults$Expression.Level <- as.factor(TractusResults$Expression.Level)
 TractusResults$Predicted.Expression.Level <- as.factor(TractusResults$Predicted.Expression.Level)
+
+# Set the plot mode to "jitter" - more aesthetically pleasing, less clear
 p <- ggplot(TractusResults, aes(x=Expression.Level, y=Expression.Value, color=Predicted.Expression.Level)) + 
     geom_jitter(position=position_jitter(0.2))
+
+# Set the plot mode to "dodge" - clear
+#p <- ggplot(TractusResults, aes(x=Expression.Level, y=Expression.Value, color=Predicted.Expression.Level)) + 
+#    geom_jitter(position=position_dodge(0.8))
+
+# Change the color scheme
 p + scale_color_brewer(palette="Dark2")
 
 
